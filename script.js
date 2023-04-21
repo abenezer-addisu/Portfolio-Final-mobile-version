ae7cc69c60a82417151211a32e36517424e5ae05
Abenezer Addisu <abenezeraddisu24@gmail.com>
2023-04-21 11:34:48 +0300

    JavaScript file Edited

--- script.js
+++ script.js
@@ -16,18 +16,6 @@ function toggleMenu() {
   }
 }
 
-function toggleMenu() {
-  if (menu.classList.contains('showMenu')) {
-    menu.classList.remove('showMenu');
-    closeIcon.style.display = 'none';
-    menuIcon.style.display = 'block';
-  } else {
-    menu.classList.add('showMenu');
-    closeIcon.style.display = 'block';
-    menuIcon.style.display = 'none';
-  }
-}
-
 hamburger.addEventListener('click', toggleMenu);
 
 menuItems.forEach(
